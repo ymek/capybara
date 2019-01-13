14c6f5651c33c71097379aca45db0703f859e43a
Thomas Walpole <twalpole@gmail.com>
2019-01-13 15:28:47 -0800

    Optimize Selenium style filter

--- lib/capybara/selenium/extensions/find.rb
+++ lib/capybara/selenium/extensions/find.rb
@@ -3,27 +3,47 @@
 module Capybara
   module Selenium
     module Find
-      def find_xpath(selector, uses_visibility: false, **_options)
-        find_by(:xpath, selector, uses_visibility: uses_visibility, texts: [])
+      def find_xpath(selector, uses_visibility: false, styles: nil, **_options)
+        find_by(:xpath, selector, uses_visibility: uses_visibility, texts: [], styles: styles)
       end
 
-      def find_css(selector, uses_visibility: false, texts: [], **_options)
-        find_by(:css, selector, uses_visibility: uses_visibility, texts: texts)
+      def find_css(selector, uses_visibility: false, texts: [], styles: nil, **_options)
+        find_by(:css, selector, uses_visibility: uses_visibility, texts: texts, styles: styles)
       end
 
     private
 
-      def find_by(format, selector, uses_visibility:, texts:)
+      def find_by(format, selector, uses_visibility:, texts:, styles:)
         els = find_context.find_elements(format, selector)
         els = filter_by_text(els, texts) if (els.size > 1) && !texts.empty?
         visibilities = uses_visibility && els.size > 1 ? element_visibilities(els) : []
-        els.map.with_index { |el, idx| build_node(el, visible: visibilities[idx]) }
+        styles = if styles.is_a?(Hash)
+          element_styles(els, styles.keys)
+        else
+          []
+        end
+        els.map.with_index { |el, idx| build_node(el, visible: visibilities[idx], style: styles[idx]) }
       end
 
       def element_visibilities(elements)
         es_context = respond_to?(:execute_script) ? self : driver
         es_context.execute_script <<~JS, elements
           return arguments[0].map(#{is_displayed_atom})
+        JS
+      end
+
+      def element_styles(elements, styles)
+        es_context = respond_to?(:execute_script) ? self : driver
+        es_context.execute_script <<~JS, elements, styles
+          var elements = arguments[0];
+          var styles = arguments[1];
+          return elements.map(function(el){
+            var el_styles = window.getComputedStyle(el);
+            return styles.reduce(function(res, style){
+              res[style] = el_styles[style];
+              return res;
+            }, {})
+          })
         JS
       end
 
